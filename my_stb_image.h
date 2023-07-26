1f1f91387f8c6c241bb01a42dce4e2f8d299786b
Jan Deinhard <jan.deinhard@gmail.com>
2023-07-26 17:57:36 +0200

    use #include <> to include STB headers

--- my_stb_image.h
+++ my_stb_image.h
@@ -17,10 +17,10 @@
 #pragma clang diagnostic ignored "-Wdeprecated-declarations"
 
 #define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
+#include <stb_image.h>
 
 #define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "stb_image_write.h"
+#include <stb_image_write.h>
 
 #pragma GCC diagnostic pop
 #pragma clang diagnostic pop
